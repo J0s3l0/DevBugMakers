1d013f251213418b5f63c2641b3884a8480ef6e9
rafacorrea <rafael_correa_3@hotmail.com>
2015-09-22 22:50:44 -0500

    Sprites, parte del UI

---
+++ BugMakers/Assets/Objetos/Scripts/BarraDeVida.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarraDeVida : MonoBehaviour {
+	public RectTransform healthTransform;
+	private float cachedY;
+	private float maxValuePos;
+	private float minValuePos;
+	private int currentHealth;
+	public int maxHealth;
+	// Use this for initialization
+	void Start () {
+		maxHealth = 100;
+		healthTransform = this.GetComponent<RectTransform>();
+		cachedY = healthTransform.position.y;
+		maxValuePos = healthTransform.position.x;
+		minValuePos = healthTransform.position.x - healthTransform.rect.width;
+		currentHealth = maxHealth;
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		healthTransform.position = new Vector3(((maxValuePos - minValuePos) / 100) * currentHealth + minValuePos, cachedY);
+		if(Input.GetKeyDown(KeyCode.Y))
+			currentHealth-=1;
+		if(Input.GetKeyDown(KeyCode.U))
+			currentHealth+=1;
+
+	}
+}
