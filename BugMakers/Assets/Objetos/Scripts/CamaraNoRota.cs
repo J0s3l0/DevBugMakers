64d66f7eb7a824754303c784ac4c8f58a698f239
rafacorrea <a01019498@2105-23.svcs.itesm.mx>
2015-09-24 12:43:14 -0500

    minimapas

---
+++ BugMakers/Assets/Objetos/Scripts/CamaraNoRota.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamaraNoRota : MonoBehaviour {
+	public GameObject target;
+	float posX;
+	float posZ;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		posX = target.transform.position.x;
+		posZ = target.transform.position.z;
+		transform.position = new Vector3(posX, transform.position.y, posZ);
+		//transform.position = posZ;
+
+	}
+}
