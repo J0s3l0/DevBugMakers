8ccbe96051d293cc1f3cf3d7bfbad820a1ec3f1d
rodericksolana <rodericksolana@gmail.com>
2015-09-23 19:24:23 -0500

    last

---
+++ BugMakers/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevel : MonoBehaviour {
+
+    // Use this for initialization
+    public string level;
+
+    // Update is called once per frame
+   public void LoadLevel1(string x)
+    {
+        Application.LoadLevel(x);
+    }
+}
--- BugMakers/Assets/Scripts/mov.cs
+++ BugMakers/Assets/Scripts/mov.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class mov : MonoBehaviour {
     // Update is called once per frame
     Vector3 nuev = new Vector3(0, 1, 0);
-    Vector3 regresa;
+    
     void Update () {
         if (Input.GetKey(KeyCode.W))
         {
@@ -16,14 +16,14 @@ public class mov : MonoBehaviour {
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            if (transform.eulerAngles.x > 330 || transform.eulerAngles.x + 4 < 30)
+            if (transform.eulerAngles.x > 345 || transform.eulerAngles.x + 4 < 15)
             {
                 transform.RotateAround(transform.position, transform.right, 1.5f);
             }
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            if(this.transform.eulerAngles.x < 30 || transform.eulerAngles.x - 4 > 330)
+            if(this.transform.eulerAngles.x < 15 || transform.eulerAngles.x - 4 > 345)
             {
                 transform.RotateAround(transform.position, -transform.right, 1.5f);
             }
@@ -31,11 +31,30 @@ public class mov : MonoBehaviour {
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.RotateAround(transform.position, nuev, 3.0f);
+            if (this.transform.eulerAngles.z > 320|| this.transform.eulerAngles.z < 45)
+                transform.RotateAround(transform.position, transform.forward, -1.5f);
 
+
+        }
+        else
+        {
+            if(transform.eulerAngles.z>315)
+            transform.RotateAround(transform.position, transform.forward, +1.5f);
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.RotateAround(transform.position, nuev, -3.0f);
+            if(this.transform.eulerAngles.z > 315 || this.transform.eulerAngles.z < 40)
+            transform.RotateAround(transform.position, transform.forward, 1.5f);
         }
+        else
+        {
+            if (transform.eulerAngles.z < 45)
+                transform.RotateAround(transform.position, transform.forward, -1.5f);
+        }
+
+        transform.Translate(new Vector3(0f, -.35f, 0f));
+
+
     }
 }
