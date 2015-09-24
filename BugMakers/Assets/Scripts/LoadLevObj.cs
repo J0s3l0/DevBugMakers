32ebdeb70fa3e755cc0cd46570647840ce42bcb4
rodericksolana <rodericksolana@gmail.com>
2015-09-24 02:06:56 -0500

    Menu Hecho!

---
+++ BugMakers/Assets/Scripts/LoadLevObj.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevObj : MonoBehaviour {
+    // Use this for initialization
+     public string level;
+    public int opcion;
+     // Update is called once per frame
+     public void LoadLevel1(string x)
+     {
+       // creaJugador.op = 1;
+         Application.LoadLevel(x);
+     }
+    public void op(int a)
+    {
+        if(a==1)
+        creaJugador.op = 1;
+        else if(a==2)
+                creaJugador.op = 2;
+    }
+   
+}
---
+++ BugMakers/Assets/Scripts/creaJugador.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class creaJugador : MonoBehaviour {
+    public static int op = 0;
+    public Transform prefab1;
+    public Transform prefab2;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (op == 1)
+        {
+
+        for (int i = 0; i < 1; i++)
+        {
+            Instantiate(prefab1, new Vector3(469.4584F, 60F, 324F), Quaternion.identity);
+        }
+            op = 0;
+    }
+        else if (op == 2)
+        {
+
+            for (int i = 0; i < 1; i++)
+            {
+                Instantiate(prefab2, new Vector3(469.4584F, 60F, 324F), Quaternion.identity);
+            }
+            op = 0;
+        }
+    }
+	
+}
