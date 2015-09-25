ed5c5e61b2e4171b817cda3bb1b946766fdb6eed
rodericksolana <rodericksolana@gmail.com>
2015-09-25 14:59:51 -0500

    borra

--- BugMakers/Assets/Scripts/test.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class test : MonoBehaviour {
-	// Update is called once per frame
-	void Update () {
-        //transform.Translate(1f, 0, 0);
-	}
-}
