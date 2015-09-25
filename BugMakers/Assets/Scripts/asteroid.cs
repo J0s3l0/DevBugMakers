53d6d81e70bfef430d61267d1696ddddee6617d7
rodericksolana <rodericksolana@gmail.com>
2015-09-24 19:45:55 -0500

    asteroides explotan

---
+++ BugMakers/Assets/Scripts/asteroid.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class asteroid : MonoBehaviour {
+
+    private int cont = 0;
+    private int posX = 0;
+    private int posZ = 0;
+    public Transform asteroid1;
+    public Transform asteroid2;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cont++;
+        if (cont%11 == 0 )
+        {   
+            for (int i = 0; i < 5; i++)
+            {
+                posX = (int)Random.Range(200, 4000);
+                posZ = (int)Random.Range(-2500, 2400);
+                Instantiate(asteroid1, new Vector3(posX, 200,posZ), transform.rotation);
+            }
+            cont = 0;
+        }
+      
+    }
+}
